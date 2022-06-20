2464683156550d233ad62ba51fee37a375b19a9b
matheusbalduino <matheus.balduin@gmail.com>
2022-06-20 11:39:40 -0300

    update tutor

--- app/Controllers/Http/TutorController.js
+++ app/Controllers/Http/TutorController.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const { HttpException } = use('@adonisjs/generic-exceptions');
 const Tutor = use('App/Models/Tutor');
+const User = use('App/Models/User');
+const Database = use('Database');
 
 class TutorController{
   /**
@@ -27,16 +30,16 @@ class TutorController{
 
     const data = request.only([
       'avatar',
-      "document",
-      "email",
+      'document',
+      'email',
       'description'
     ]);
 
     const dataUser = request.only([
-      "username",
-      "password",
-      "role",
-      "profession"
+      'username',
+      'password',
+      'role',
+      'profession'
     ]);
 
     try {
@@ -52,6 +55,53 @@ class TutorController{
     catch(error)
     {
       throw error;
+    }
+  }
+
+  async update({request, response, params}){
+    try{
+      const {id} = params;
+
+      const dataTutor = request.only(['avatar', 'document', 'email', 'description']);
+      const dataUser = request.only(['username', 'password', 'role', 'profession']);
+
+      const tutor = await Tutor.findBy('id', id);
+      if(!tutor) throw new Error('Tutor não existe');
+
+      const user = await User.findBy('tutor_id', id);
+      if(!user) throw new Error('Usuário não existe');
+
+      const trx = await Database.beginTransaction();
+
+      try {
+        dataTutor.avatar = (dataTutor.avatar)?dataTutor.avatar : tutor.avatar;
+        dataTutor.document = (dataTutor.document)?dataTutor.document : tutor.document;
+        dataTutor.email = (dataTutor.email)? dataTutor.email :tutor.email;
+        dataTutor.description = (dataTutor.description)?dataTutor.description : tutor.description;
+
+        tutor.merge(dataTutor);
+        await tutor.save(trx);
+
+        dataUser.username = dataUser.username ? dataUser.username : user.username;
+        dataUser.password = dataUser.password ? dataUser.password : user.password;
+        dataUser.role = dataUser.role ? dataUser.role : user.role;
+        dataUser.profession = dataUser.profession ? dataUser.profession : user.profession;
+        dataUser.tutor_id = 3;
+
+        user.merge(dataUser);
+        await user.save(trx);
+
+        await trx.commit();
+
+      } catch (error) {
+        await trx.rollback();
+        throw error;
+      }
+
+      return response.send({updated: tutor});
+
+    }catch(error){
+      throw new HttpException(error.message, 400);
     }
   }
 }
--- start/routes/tutor.js
+++ start/routes/tutor.js
@@ -4,4 +4,5 @@ const Route = use('Route');
 
 Route.group(()=>{
   Route.post('/store', 'TutorController.store');
+  Route.put('/update/:id', 'TutorController.update');
 }).prefix('tutor')
