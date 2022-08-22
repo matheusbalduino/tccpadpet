7af018aaa340f91cf3c30271aa973db15a788bdc
matheusbalduino <matheus.balduin@gmail.com>
2022-08-22 10:30:14 -0300

    commit button

--- Frontend/src/app/cadastros/cadastros.module.ts
+++ Frontend/src/app/cadastros/cadastros.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CadastroTutorComponent } from './cadastro-tutor/cadastro-tutor.component';
 import { MaterialModule } from 'material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CadastroService } from '../Services/cadastro-service.service';
 
 
 
